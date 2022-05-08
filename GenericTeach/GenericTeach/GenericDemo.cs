9254bbdc2afa69ab809c18a04d6d9417a2ff48d3
Ken Zhang <feeel@shaw.ca>
2022-05-07 18:19:07 -0600

    Generic type, constraint, dynamic, default ...

---
+++ GenericTeach/GenericTeach/GenericDemo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericTeach
+{
+
+	/// <summary>
+	/// a class demo for stack push and pop data
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class MyStack<T>
+	{
+		//prepare array type data
+		private T[] stack;
+
+		// initial start position of elements
+		private int stackPoint;
+
+		// initial the size of the stack
+		private int size;
+
+		// constructor
+		public MyStack(int size)
+		{
+			this.size = size;
+			this.stack = new T[size];
+			this.stackPoint = -1; // stack starts at 0, so the first element index would be -1+1 = 0
+		}
+
+		// put data into stack
+		public void Push(T data)
+		{
+			if (stackPoint >= size) Console.WriteLine("Stack is full!");
+			else
+			{
+				stackPoint++;
+				
+				this.stack[stackPoint] = data;
+			}
+		}
+		/// <summary>
+		/// take element out from stack
+		/// </summary>
+		/// <returns></returns>
+
+		public T Pop()
+		{
+			T data = this.stack[stackPoint];
+			this.stack[stackPoint] = default(T); //default keyword is for directly give value for generic type.
+			stackPoint--;
+			return data;
+        }
+    }
+
+	#region Generic type 'default' keyword
+	public class MyGenericClass<T1, T2> 
+	{
+		private T1 data1;
+		private T2 data2;
+		private T2 data3;
+
+		public MyGenericClass()
+        {
+			//data1 = new T1(); //error CS0304: Cannot create an instance of the variable type 'T1' because it does not have the new() constraint
+			//data2 = null; // error CS0403: Cannot convert null to type parameter 'T2' because it could be a non-nullable value type. Consider using 'default(T2)' instead.
+			data3 = default(T2);
+        }
+	}
+    #endregion
+
+    #region Generic type constraint and dynamic type
+	public class MyGenericClassConstraint<T1, T2, T3>
+		where T1: struct // T1 have to be a value type.
+		where T2: class // T2 have to be a reference type.
+		where T3:new() // T3 type must have a no argument constrctor, and this constraint must be put on the end of line
+
+		//other constraint: base , interface ...
+    {
+        public List<T2> ProductList { get; set; }
+        public T3 Publisher { get; set; }
+
+		public MyGenericClassConstraint()
+        {
+			this.ProductList = new List<T2>();
+			Publisher = new T3(); //Cannot create an instance of the variable type 'type' because it does not have the new() constraint
+		}
+
+
+		// for certain type it need to be dynamic to be valid
+		public T2 BuyCourse(T1 position)
+        {
+			//return this.ProductList[position]; //error,position need to be int type not generic. use dynamic instead
+			dynamic index = position;
+			return this.ProductList[index];
+        }
+
+
+    }
+
+
+		public class Course
+        {
+			public string CourseName { get; set; }
+            public int PeriodLasts { get; set; }
+        }
+
+		public class Teacher
+        {
+			public Teacher() { }
+			public string Name { get; set; }
+			public	int CourseCount { get; set; }
+        }
+
+
+    #endregion
+
+}
---
+++ GenericTeach/GenericTeach/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GenericTeach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMyStack();
+            Console.WriteLine("********************************");
+            TestMyGenericConstraint();
+            Console.ReadLine();
+        }
+
+        static void test()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add("Good day!");
+        }
+        /*
+        static T3 Add<T1, T2, T3>(T1 a, T2 b)
+        {
+            return a + b;
+        }*/
+
+        static void TestMyStack()
+        {
+            // create a MyStack instance with type int size 5
+            MyStack<int> stack1 = new MyStack<int>(5);
+           
+            //push item into stack
+            stack1.Push(1);
+            stack1.Push(2);
+            Console.WriteLine(stack1);
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Push(5);
+            Console.WriteLine(stack1);
+            // pop item out from stack
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+
+            MyStack<string> stack2 = new MyStack<string>(5);
+
+            stack2.Push("Generic type");
+            stack2.Push("C# programming");
+            stack2.Push("ASP.net MVC");
+            stack2.Push("reflection");
+            stack2.Push("test compelete!");
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+
+        }
+
+        /// <summary>
+        /// test generic constraint 
+        /// </summary>
+        static void TestMyGenericConstraint()
+        {
+            MyGenericClassConstraint<int, Course, Teacher> myGeneric = new MyGenericClassConstraint<int, Course, Teacher>();
+            myGeneric.Publisher = new Teacher { Name = "Mr. Tom", CourseCount = 3 };
+            myGeneric.ProductList = new List<Course> {
+                new Course {
+                    CourseName = "C# fundamentals",
+                    PeriodLasts = 4 //4 months course
+                },
+                new Course
+                {
+                    CourseName = ".NET Full Stack",
+                    PeriodLasts = 4 //4 months course
+                },
+                new Course
+                {
+                    CourseName = "ASP.NET Frontend",
+                    PeriodLasts = 3 //3 months course
+                }
+            };
+
+            var myCourse = myGeneric.BuyCourse(1);
+
+            Console.WriteLine($"The course I bought is {myCourse.CourseName}, lasts about {myCourse.PeriodLasts} months, Teacher is {myGeneric.Publisher.Name}.");
+        }
+    }
+}
